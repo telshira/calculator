80359e29ddc9af6f2402a5e01c9f927d709dd6e5
Thu Nguyen <telshira79@gmail.com>
2022-08-29 16:35:22 -0700

    Add new classes and values to calc-button for easier access to buttons. Set up functions to get values currently used for calculations for display. Set up function to do +/-* operations. Set up delete, clear current number and clear all buttons.

--- main.js
+++ main.js
@@ -21,15 +21,160 @@ function toggleOnOff(e) {
     screen.setAttribute('style', 'background:black; opacity: 75%');
     resultScrn.textContent = "";
     operatorScrn.textContent= "";
+    resetCalc();
     resultScrn.removeAttribute('style');
     onOffBtn.removeAttribute('style');
     onOffBtn.innerHTML = '<span> ON </span>';
     let onOffBtnSpan = document.querySelector('#onOffBtn > span');
     onOffBtnSpan.removeAttribute('style');
-    
+  }
+}
+onOffBtn.addEventListener('click', toggleOnOff, false);
+
+const calcButtons = document.querySelectorAll('.numbers, .operatorBtn');
+const operatorBtn = document.querySelectorAll('.operatorBtn');
+const numbersBtn = document.querySelectorAll('.numbers');
+const deleteBtn = document.getElementById('deleteBtn');
+const clearAllBtn = document.getElementById('clearAllBtn');
+const clearCurrBtn = document.getElementById('clearCurrBtn');
+
+let result;
+let numbers = [];
+let operator = [];
+let tempArray = [];
+let keyArray = [];
+let isOperator = false;
+let decimalPressed = false;
+let keyValue = '';
+
+function resetCalc() {
+  numbers = [];
+  operator = [];
+  tempArray = [];
+  keyArray = [];
+  isOperator = false;
+  decimalPressed = false;
+  result = '';
+  resultScrn.textContent = '';
+  operatorScrn.textContent = '';
+}
+
+function disableOper() {
+  operatorBtn.forEach(button => button.disabled = true);
+  isOperator = true;
+  keyArray.push(this.value);
+  return keyValue = this.value;
+}
+
+operatorBtn.forEach(button => button.addEventListener('click', disableOper));
+numbersBtn.forEach(button => button.addEventListener('click', () => {
+  operatorBtn.forEach(button => button.disabled = false);
+}))
+calcButtons.forEach(button => button.addEventListener('click', (e) => {
+  e.preventDefault();
+  getNumbersForCalc(e);
+  if (numbers[1]){
+    operations();
+    operator.shift();
+    keyArray.shift();
+    numbers = [];
+    numbers.push(result);
+    resultScrn.setAttribute('style', 'color: black; opacity: 1');
+    resultScrn.textContent = result;
+    result = '';
+    console.log(result);
+  }
+})); 
+
+
+function getNumbersForCalc(e){
+  if (isPower) {
+    if (tempArray.length < Infinity) {
+      let tempNum = 0;
+      if (tempArray[1] && tempArray[0] ==='0')
+        tempArray.shift();
+      else 
+        tempArray.push(e.target.textContent);
+      if (isOperator) {
+        if (tempArray[0]) {
+          tempNum = tempArray.slice(0, tempArray.length-1).join("");
+          numbers.push(tempNum);
+        }
+        isOperator = false;
+        let popped = tempArray.pop();
+        operator.push(popped);
+        tempArray.length = 0;
+      } else 
+        changeTopScreen();
+    }
   }
 }
 
-onOffBtn.addEventListener('click', toggleOnOff);
+//put current calculations on top display
+function changeTopScreen() {
+  if (!numbers[0]) {
+    if (tempArray[0] === '0')
+      tempArray.pop();
+    else
+      operatorScrn.textContent = tempArray.join("");
+  } else {
+    if (numbers[0]) {
+      operatorScrn.innerHTML = `${numbers[0]} ${operator[0]} ${tempArray.join("")} =`;
+    } else {
+      operatorScrn.textContent = tempArray.join("");
+    }
+  }
+}
 
+function deleteLast() {
+  if (tempArray[0]){
+    tempArray.pop();
+    changeTopScreen();
+  }
+}
 
+function deleteCurr() {
+  tempArray = [];
+  changeTopScreen();
+}
+
+deleteBtn.addEventListener('click', deleteLast);
+clearCurrBtn.addEventListener('click', deleteCurr);
+clearAllBtn.addEventListener('click', resetCalc);
+
+function operations(){
+  if(isPower){
+    switch(keyArray[0]){
+      case "+":
+          if (result) 
+            result =+ parseFloat(numbers[1]);
+          else if (numbers[1])
+            result = (parseFloat(numbers[0]) + parseFloat(numbers[1])).toFixed(2);
+        break
+       
+      case "-":
+        if (numbers[1])
+          if (result) 
+            result =- parseFloat(numbers[1]);
+          else if (numbers[1])
+            result = (parseFloat(numbers[0]) - parseFloat(numbers[1])).toFixed(2);
+        break;
+
+       case "*":
+        if (numbers[1])
+          if (result) 
+            result *= parseFloat(numbers[1]);
+          else if (numbers[1])
+            result = ((parseFloat(numbers[0]) * parseFloat(numbers[1]))).toFixed(2);
+        break;
+
+       case "/":
+        if (numbers[1])
+          if (result) 
+            result = (result/parseFloat(numbers[1])).toFixed(2);
+          else if (numbers[1])
+            result = (parseFloat(numbers[0])/parseFloat(numbers[1])).toFixed(2);
+        break;
+      }
+    }
+  }
