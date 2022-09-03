6f4f8dc51a3d63acb730aa37f3d81b24f25f6404
Thu Nguyen <telshira79@gmail.com>
2022-09-02 22:28:02 -0700

    Merged similar style of buttons together. Added function for square root operation and adding decimal to numbers. Updated operations to include X to the N power and what happens when dividing with 0. Edited changeTopScreen to show blank screen when nothing is typed or when number has been cleared.

--- main.js
+++ main.js
@@ -19,8 +19,6 @@ function toggleOnOff(e) {
   } else {
     isPower = false;
     screen.setAttribute('style', 'background:black; opacity: 75%');
-    resultScrn.textContent = "";
-    operatorScrn.textContent= "";
     resetCalc();
     resultScrn.removeAttribute('style');
     onOffBtn.removeAttribute('style');
@@ -37,73 +35,88 @@ const numbersBtn = document.querySelectorAll('.numbers');
 const deleteBtn = document.getElementById('deleteBtn');
 const clearAllBtn = document.getElementById('clearAllBtn');
 const clearCurrBtn = document.getElementById('clearCurrBtn');
+const decimalBtn = document.getElementById('decimalBtn');
 
-let result;
-let numbers = [];
-let operator = [];
-let tempArray = [];
-let keyArray = [];
+let result = '';
+const numbers = [];
+const operator = []; //operator for display screen
+const tempArray = [];
+const keyArray = []; //operator used in operations
 let isOperator = false;
-let decimalPressed = false;
 let keyValue = '';
 
 function resetCalc() {
-  numbers = [];
-  operator = [];
-  tempArray = [];
-  keyArray = [];
+  numbers.length = 0;
+  operator.length = 0;
+  tempArray.length = 0;
+  keyArray.length = 0;
   isOperator = false;
-  decimalPressed = false;
   result = '';
-  resultScrn.textContent = '';
+  resultScrn.textContent = '0';
   operatorScrn.textContent = '';
+  numbersBtn.forEach(button => button.disabled = false);
+  decimalBtn.disabled = false;
 }
 
 function disableOper() {
   operatorBtn.forEach(button => button.disabled = true);
+  numbersBtn.forEach(button => button.disabled = false);
   isOperator = true;
+  decimalBtn.disabled = false;
   keyArray.push(this.value);
+  if (keyArray[0] === "="){
+    keyArray.shift();
+  }
   return keyValue = this.value;
 }
 
 operatorBtn.forEach(button => button.addEventListener('click', disableOper));
 numbersBtn.forEach(button => button.addEventListener('click', () => {
   operatorBtn.forEach(button => button.disabled = false);
+  decimalBtn.disabled = false;
+  isOperator = false;
 }))
 calcButtons.forEach(button => button.addEventListener('click', (e) => {
   e.preventDefault();
   getNumbersForCalc(e);
   if (numbers[1]){
     operations();
-    operator.shift();
-    keyArray.shift();
-    numbers = [];
-    numbers.push(result);
-    resultScrn.setAttribute('style', 'color: black; opacity: 1');
-    resultScrn.textContent = result;
-    result = '';
-    console.log(result);
-  }
-})); 
+    updateResult();
+    }
+}));
 
+function updateResult(){
+  operator.shift();
+  keyArray.shift();
+  numbers.length = 0;
+  if (result.length > 14)  // so result will not go past bottom screen container
+  result = parseFloat(result).toPrecision(8);
+  numbers.push(result);
+  resultScrn.setAttribute('style', 'color: black; opacity: 1');
+  resultScrn.textContent = result;
+  result = '';
+}
 
 function getNumbersForCalc(e){
   if (isPower) {
     if (tempArray.length < Infinity) {
-      let tempNum = 0;
-      if (tempArray[1] && tempArray[0] ==='0')
-        tempArray.shift();
-      else 
-        tempArray.push(e.target.textContent);
+      let tempNum;
+      tempArray.push(e.target.textContent);
       if (isOperator) {
-        if (tempArray[0]) {
+        if (tempArray) {
           tempNum = tempArray.slice(0, tempArray.length-1).join("");
-          numbers.push(tempNum);
+          if (tempNum !== "")  
+            numbers.push(tempNum);
+          if (keyValue === "="){
+            operatorBtn.forEach(button => button.disabled = false);
+            numbersBtn.forEach(button => button.disabled = true);
+            decimalBtn.disabled = true;
+          } else {
+            let popped = tempArray.pop();
+            if (popped != "=") operator.push(popped);
+          }
+          tempArray.length = 0;
         }
-        isOperator = false;
-        let popped = tempArray.pop();
-        operator.push(popped);
-        tempArray.length = 0;
       } else 
         changeTopScreen();
     }
@@ -113,28 +126,30 @@ function getNumbersForCalc(e){
 //put current calculations on top display
 function changeTopScreen() {
   if (!numbers[0]) {
-    if (tempArray[0] === '0')
-      tempArray.pop();
-    else
-      operatorScrn.textContent = tempArray.join("");
-  } else {
-    if (numbers[0]) {
-      operatorScrn.innerHTML = `${numbers[0]} ${operator[0]} ${tempArray.join("")} =`;
-    } else {
-      operatorScrn.textContent = tempArray.join("");
-    }
-  }
+    if (tempArray.length === 0)
+      operatorScrn.textContent = ''; 
+    else 
+      operatorScrn.textContent = parseFloat(tempArray.join(""));
+  } else if (numbers[0] && operator[0] && tempArray[0]) {
+    operatorScrn.textContent = `${parseFloat(numbers[0])} ${operator[0]} ${parseFloat(tempArray.join(""))} =`; 
+  } else if (numbers[0] && operator[0])
+  operatorScrn.textContent = `${parseFloat(numbers[0])} ${operator[0]} =`
 }
 
 function deleteLast() {
-  if (tempArray[0]){
+  if (tempArray){
     tempArray.pop();
     changeTopScreen();
   }
 }
 
 function deleteCurr() {
-  tempArray = [];
+  if (tempArray.length === 0){ //delete current operation if messed up
+    operator.length = 0;
+    keyArray.length = 0;
+  } else {
+    tempArray.length = 0;
+  }
   changeTopScreen();
 }
 
@@ -146,35 +161,62 @@ function operations(){
   if(isPower){
     switch(keyArray[0]){
       case "+":
-          if (result) 
-            result =+ parseFloat(numbers[1]);
-          else if (numbers[1])
-            result = (parseFloat(numbers[0]) + parseFloat(numbers[1])).toFixed(2);
-        break
+        result = (parseFloat(numbers[0]) + parseFloat(numbers[1])).toFixed(3);
+      break
        
       case "-":
-        if (numbers[1])
-          if (result) 
-            result =- parseFloat(numbers[1]);
-          else if (numbers[1])
-            result = (parseFloat(numbers[0]) - parseFloat(numbers[1])).toFixed(2);
-        break;
+        result = (parseFloat(numbers[0]) - parseFloat(numbers[1])).toFixed(3);
+      break;
 
-       case "*":
-        if (numbers[1])
-          if (result) 
-            result *= parseFloat(numbers[1]);
-          else if (numbers[1])
-            result = ((parseFloat(numbers[0]) * parseFloat(numbers[1]))).toFixed(2);
-        break;
+      case "*":
+        result = ((parseFloat(numbers[0]) * parseFloat(numbers[1]))).toFixed(3);
+      break;
 
-       case "/":
-        if (numbers[1])
-          if (result) 
-            result = (result/parseFloat(numbers[1])).toFixed(2);
-          else if (numbers[1])
-            result = (parseFloat(numbers[0])/parseFloat(numbers[1])).toFixed(2);
-        break;
-      }
+      case "/":
+        if (numbers[1] === "0") {  //resets calculator after 1.5s if error from dividing by 0
+          result = "Error"
+          setTimeout(() => {
+            resetCalc();
+          }, 1500);
+        } else 
+          result = ((parseFloat(numbers[0])/parseFloat(numbers[1])).toFixed(3));
+      break;
+
+      case "nPower":
+        result = Math.pow(parseFloat(numbers[0]), parseFloat(numbers[1])).toFixed(3);
+      break;
+     }
     }
   }
+
+const sqRootBtn = document.getElementById('sqRootBtn');
+function sqRoot() {
+  if (!numbers[0])
+   return;
+  else {
+    operatorScrn.innerHTML = `${sqRootBtn.textContent}${numbers[0]}`;
+    result = Math.sqrt(parseFloat(numbers[0])).toFixed(3);
+    if (result === "NaN") {
+      resultScrn.textContent = "NaN";
+      setTimeout(() => {
+        resetCalc();
+      }, 1500);
+    } else {
+      tempArray.length = 0;
+      operatorBtn.forEach(button => button.disabled = false);
+      numbersBtn.forEach(button => button.disabled = true);
+      decimalBtn.disabled = true;
+      updateResult();
+    }
+  }
+}
+sqRootBtn.addEventListener('click', sqRoot);
+
+decimalBtn.addEventListener('click', () =>{
+  decimalBtn.disabled = true;
+  isOperator = false;
+  if (tempArray.includes(".")){
+    return;
+  } else
+    tempArray.push(".");
+})
